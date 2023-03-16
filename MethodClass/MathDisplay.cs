ce1c75b69363b16f119f8c517c2c186ed4e69da6
Arieleon25 <Arieleon25@yahoo.com>
2023-03-16 12:44:26 -0400

    Add project files.

---
+++ MethodClass/MathDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    public class MathDisplay
+    {
+        public void MathDis (int i, int j)
+        {
+            int num1 = i + 5;
+            int num2 = j;
+
+            Console.WriteLine("{0}, {1}", num1, num2);
+        }
+    }
+}
---
+++ MethodClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathDisplay mathDisplay = new MathDisplay();
+
+            mathDisplay.MathDis(5, 7);
+
+            Console.ReadLine();
+        }
+    }
+}
